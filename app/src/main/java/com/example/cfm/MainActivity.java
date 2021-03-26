6862d01f19f8eabe031fa4516507d13f7cd87163
David Ye <dly8@case,edu>
2021-03-26 07:33:04 -0400

    location permissions
    Signed-off-by: David Ye <dly8@case,edu>

--- app/src/main/java/com/example/cfm/MainActivity.java
+++ app/src/main/java/com/example/cfm/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.cfm;
 
+import android.Manifest;
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -12,6 +24,7 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.R)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +38,30 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        checkPermissions(AppOpsManager.OPSTR_COARSE_LOCATION, Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        testLocation();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void checkPermissions(String permission, String setting) {
+        System.out.println(permission + "\t" + setting);
+        AppOpsManager appOps = (AppOpsManager) getSystemService(Context.APP_OPS_SERVICE);
+        if (appOps.checkOpNoThrow(permission, android.os.Process.myUid(), getPackageName()) == AppOpsManager.MODE_ALLOWED)
+            System.out.println("we do have permission");
+        else startActivityForResult(new Intent(setting), 69);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    public void testLocation() {
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String provider = lm.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("LOCATION CHECK FAILING FOR SOME REASON?!?!?!?!");
+            return;
+        }
+        Location l = lm.getLastKnownLocation(provider);
     }
 
 }

d7e51d0f44aea1b4eda18feb1fc2b8385705bca0
Javon Colvin-Hatchett <43857131+Javonch@users.noreply.github.com>
2021-03-31 14:06:18 -0400

    resetting repo

--- app/src/main/java/com/example/spotify_framework/User.java
+++
@@ -1,9 +0,0 @@
-package com.example.spotify_framework;
-
-public class User {
-    public String birthdate;
-    public String country;
-    public String display_name;
-    public String email;
-    public String id;
-}
